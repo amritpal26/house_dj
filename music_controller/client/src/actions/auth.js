9052d9f86c958204b19fe8c692065d23db63e503
Amritpal Singh <amritpal.am26@gmail.com>
2021-04-07 01:09:24 -0700

    add authentication actions and reducers

--- music_controller/client/src/actions/auth.js
+++ music_controller/client/src/actions/auth.js
@@ -1,5 +1,104 @@
 import axios from 'axios';
+import ActionTypes from './actionTypes';
 import actionTypes from './actionTypes';
+
+const URL_RETREIVE_AUTHENTICATED_USER = '/auth/users/me/';
+const URL_GOOGLE_OAUTH_2 = '/auth/o/google-oauth2/';
+const URL_CHECK_USER_AUTHENTICATED_JWT = '/auth/jwt/verify/';
+
+export const loadUser = () => async dispatch => {
+    if (localStorage.getItem('accessToken')) {
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `JWT ${localStorage.getItem('accessToken')}`,
+                'Accept': 'application/json'
+            }
+        };
+
+        try {
+            const res = await axios.get(`${process.env.REACT_APP_API_URL}${URL_RETREIVE_AUTHENTICATED_USER}`, config);
+    
+            dispatch({
+                type: actionTypes.authActions.USER_LOAD_SUCCESS,
+                payload: res.data
+            });
+        } catch (err) {
+            dispatch({
+                type: actionTypes.authActions.USER_LOAD_FAIL
+            });
+        }
+    } else {
+        dispatch({
+            type: actionTypes.authActions.USER_LOAD_FAIL
+        });
+    }
+};
+
+export const checkAuthenticated = () => async dispatch => {
+    if (localStorage.getItem('accessToken')) {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }; 
+        const body = JSON.stringify({ token: localStorage.getItem('accessToken') });
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}${URL_CHECK_USER_AUTHENTICATED_JWT}`, body, config)
+            if (res.data.code !== 'token_not_valid') {
+                dispatch({
+                    type: actionTypes.authActions.AUTHENTICATION_SUCCESS
+                });
+            } else {
+                dispatch({
+                    type: actionTypes.authActions.AUTHENTICATION_FAIL
+                });
+            }
+        } catch (err) {
+            dispatch({
+                type: actionTypes.authActions.AUTHENTICATION_FAIL
+            });
+        }
+    } else {
+        dispatch({
+            type: actionTypes.authActions.AUTHENTICATION_FAIL
+        });
+    }
+};
+
+export const googleAuthenticate = (state, code) => async dispatch => {
+    if (state && code && !localStorage.getItem('accessToken')) {
+        const config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        };
+
+        const details = {
+            'state': state,
+            'code': code
+        };
+
+        const formBody = Object.keys(details).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(details[key])).join('&');
+
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}${URL_RETREIVE_AUTHENTICATED_USER}?${formBody}`, config);
+            
+            dispatch({
+                type: actionTypes.authActions.GOOGLE_AUTH_SUCCESS,
+                payload: res.data
+            });
+        } catch (err) {
+            dispatch({
+                type: actionTypes.authActions.GOOGLE_AUTH_FAIL
+            });
+        }
+    } else {
+
+    }
+};
 
 export const login = (email, password) => dispatch =>  {
     console.log('called action');
@@ -7,4 +106,8 @@ export const login = (email, password) => dispatch =>  {
 
 export const signUp = (firstName, lastName, email, password) => dispatch => {
     console.log('signUp called');
+};
+
+export const logout = () => {
+    console.log('logout action called');
 };
--- music_controller/client/src/reducers/auth.js
+++ music_controller/client/src/reducers/auth.js
@@ -10,11 +10,49 @@ const initialState = {
 export default function(state = initialState, action) {
     const { type, payload } = action;
 
+    console.log(`dispatching '${type}'`, payload);
     switch(type) {
-        case actionTypes.authActions.LOGIN_SUCCESS:
+        case actionTypes.authActions.AUTHENTICATION_SUCCESS:
             return {
                 ...state,
                 isAuthenticated: true
+            }
+        case actionTypes.authActions.LOGIN_SUCCESS:
+        case actionTypes.authActions.GOOGLE_AUTH_SUCCESS:
+            localStorage.setItem('accessToken', payload.accessToken);
+            localStorage.setItem('refreshToken', payload.refreshToken);
+            return {
+                ...state,
+                isAuthenticated: true,
+                accessToken: payload.accessToken,
+                refreshToken: payload.refreshToken
+            }
+        case actionTypes.authActions.USER_LOAD_SUCCESS:
+            return {
+                ...state,
+                user: payload
+            }
+        case actionTypes.authActions.AUTHENTICATION_FAIL:
+            return {
+                ...state,
+                isAuthenticated: false
+            }
+        case actionTypes.authActions.LOGIN_FAIL:
+        case actionTypes.authActions.GOOGLE_AUTH_FAIL:
+        case actionTypes.authActions.LOGOUT:
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('refreshToken');
+            return {
+                ...state,
+                isAuthenticated: false,
+                accessToken: null,
+                refreshToken: null,
+                user: null
+            }
+        case actionTypes.authActions.USER_LOAD_FAIL:
+            return {
+                ...state,
+                user: null
             }
         default:
             return state
