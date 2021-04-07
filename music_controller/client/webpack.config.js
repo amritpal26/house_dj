53732557dc072c95757ef8b217cf609bcf503033
Amritpal Singh <amritpal.am26@gmail.com>
2021-04-07 03:19:39 -0700

    add dotenv

--- music_controller/client/webpack.config.js
+++ music_controller/client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
 	entry: "./src/index.js",
@@ -32,5 +33,6 @@ module.exports = {
 				NODE_ENV: JSON.stringify("development"),
 			},
 		}),
+		new Dotenv(),
 	],
 }
