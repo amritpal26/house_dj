d8fc0f8ab8aaf8351d4ec8c2cad2f459108a653a
Amritpal Singh <amritpal.am26@gmail.com>
2021-04-07 02:19:28 -0700

    update configs

--- music_controller/client/src/configs.js
+++ music_controller/client/src/configs.js
@@ -1,9 +1,24 @@
+import {red, blue} from "@material-ui/core/colors";
+
 const Constants = {
+    // App 
+    APP_NAME: 'House DJ',
+
+    PASSWORD_MIN_LENGTH: 6,
+
+    // Room
     DEFAULT_GUEST_CAN_PAUSE: true,
     DEFAULT_VOTES_TO_SKIP: 2,
+};
 
-    // Authentication
-    PASSWORD_MIN_LENGTH: 6
+const Colors = {
+    // Buttons
+    FACEBOOK_BTN: blue[500],
+    GOOGLE_BTN: blue[500],
+
+};
+
+export default {
+    constants: Constants,
+    colors: Colors,
 }
-
-module.exports.constants = Constants;
